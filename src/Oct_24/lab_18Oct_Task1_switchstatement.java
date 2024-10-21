bc5dbfc4d7f633e47e30e2ee4224e5f0a7f6b2c6
SAURABH <er.saurabhkumar22@gmail.com>
2024-10-21 19:56:36 +0530

    added

--- src/Oct_24/lab_18Oct_Task1_switchstatement.java
+++ src/Oct_24/lab_18Oct_Task1_switchstatement.java
@@ -1,7 +1,43 @@
 package Oct_24;
+
+import java.util.Scanner;
+
 //Create a simple calculator that performs addition, subtraction, multiplication,
 // and division, modus based on user input using switch statements.
 //Inputs :   num 1, num 2, +
 //Output :  num1+num2 → print information.
 public class lab_18Oct_Task1_switchstatement {
-}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the operation");
+        String operation = in.next();
+        System.out.println("Enter first value");
+        int num1 = in.nextInt();
+        System.out.println("Enter the Second value ");
+        int num2 = in.nextInt();
+        int result =0;
+        in.close();
+        switch (operation){
+            case "add":
+                result = num1 + num2;
+                break;
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "Divide":
+                result = num1 / num2;
+                break;
+            case "Modules":
+                result = num1 % num2;
+                break;
+            default:
+                System.out.println("Entered operator is invalid");
+        }
+        System.out.println("Result is: "+result);
+
+        }
+    }
+
