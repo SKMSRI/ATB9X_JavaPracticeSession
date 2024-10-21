0e1ca946fd12a746c4df0500a4f67c04f4177305
SAURABH <er.saurabhkumar22@gmail.com>
2024-10-21 12:03:21 +0530

    17_oct task

---
+++ src/Oct_24/lab_17Oct_triangleclassifier.java
@@ -0,0 +1,33 @@
+package Oct_24;
+//Write a program that classifies a triangle based on its side lengths.
+//Given three input values representing the lengths of the sides, determine if the triangle is equilateral (all sides are equal), isosceles (exactly two sides are equal), or scalene (no sides are equal). Use an if-else statement to classify the triangle.
+// side1, side2, side2 → eq, iso, scalene
+
+import java.util.Scanner;
+
+public class lab_17Oct_triangleclassifier {
+    public static void main(String[] args) {
+
+         Scanner sc= new Scanner(System.in);
+            System.out.println("enter the three sides of triangle");
+            int s1= sc.nextInt();
+//    System.out.println("enter the 2nd side of triangle");
+            int s2= sc.nextInt();
+//    System.out.println("enter the 3rd side of triangle");
+            int s3=sc.nextInt();
+            if(s1<=0 || s2<=0 ||s3<=0)
+            {
+                System.out.println("Invalid data, please enter the positive sides");
+            }
+//    equilateral (all sides are equal)
+            else if(s1==s2 && s1==s3){
+                System.out.println("This is an equilateral triangle");
+            } else if (s1==s2 || s1==s3 ||(s2==s3)) {
+                System.out.println("This is an isosceles Triangle");
+            }
+            else{
+                System.out.println("scalene Triangle");
+            }
+            sc.close();
+        }
+    }
---
+++ src/Oct_24/lab_17_oct_gradeCalculator.java
@@ -0,0 +1,37 @@
+package Oct_24;
+
+import java.util.Scanner;
+
+// Write a program that calculates and displays the letter grade
+// for a given numerical score (e.g., A, B, C, D, or F)
+// based on the following grading scale:
+//
+//A: 90-100
+//B: 80-89
+//C: 70-79
+//D: 60-69
+//F: 0-59
+public class lab_17_oct_gradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Score");
+        int Score = sc.nextInt();
+        if (Score >= 90 && Score <= 100) {
+            System.out.println(" Your Grade is -" + 'A');
+        } else if (Score >= 80 && Score <= 89) ;
+        {
+            System.out.println("Your Grade is -" + 'B');
+        }
+        //else if (Score >= 70 && Score <= 79) ;
+        {
+            System.out.println("Your Grade is -"+ 'C');
+        }
+   }
+
+    }
+
+
+
+
+
+
