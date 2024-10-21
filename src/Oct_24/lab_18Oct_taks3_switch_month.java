113365e5d2618ce8e436ab8be1917f563c02ab1c
SAURABH <er.saurabhkumar22@gmail.com>
2024-10-21 20:11:39 +0530

    added

--- src/Oct_24/lab_18Oct_taks3_switch_month.java
+++ src/Oct_24/lab_18Oct_taks3_switch_month.java
@@ -1,8 +1,13 @@
 package Oct_24;
+
+import java.util.Scanner;
+
 //Write a program that converts between different units (e.g., kilometers to miles,
 // Celsius to Fahrenheit) based on user selection using a switch statement.
 //Input. -
 //choice - 1 - km → m, km → 1km
 //choice - 2 - f → c, f → c
-public class lab_18Oct_taks3_switch_month {
-}
+//public class lab_18Oct_taks3_switch_month {
+    //Scanner sc = new Scanner(System.in);
+
+
---
+++ src/Oct_24/lab_18_Oct_practice_swtchstmnt_gender.java
@@ -0,0 +1,22 @@
+package Oct_24;
+
+import java.util.Scanner;
+
+public class lab_18_Oct_practice_swtchstmnt_gender {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the M/F or Other");
+        String num= in.next();
+        in.close();
+        switch (num){
+            case "M":
+                System.out.println("Male");
+                break;
+            case "F":
+                System.out.println("Female");
+                break;
+            default:
+                System.out.println("No gender specified");
+        }
+    }
+}
