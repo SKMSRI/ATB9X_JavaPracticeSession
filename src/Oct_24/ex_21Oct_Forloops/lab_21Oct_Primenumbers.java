019cbb8af9abcbc5c4d5f92681fdeb34b58e1be8
SAURABH <er.saurabhkumar22@gmail.com>
2024-10-23 16:51:42 +0530

    added

--- src/Oct_24/ex_21Oct_Forloops/lab_21Oct_Primenumbers.java
+++ src/Oct_24/ex_21Oct_Forloops/lab_21Oct_Primenumbers.java
@@ -1,10 +1,11 @@
 package Oct_24.ex_21Oct_Forloops;
+
 //Java program to print all Prime numbers between 1 to n using for loop.
-public class lab_21Oct_Primenumbers {
+//public class lab_21Oct_Primenumbers {
     public static void main(String[] args) {
-//        for (int i =1; i<n; i++){
+//        for (int i = 1; i < n; i++) {
 //            if ()
-        }
-    }
+//        }
+ }
 
 
