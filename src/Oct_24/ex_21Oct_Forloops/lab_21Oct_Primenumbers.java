f34d34131f28ecf8610e64691390eca8bd2d137c
SAURABH <er.saurabhkumar22@gmail.com>
2024-10-25 19:05:05 +0530

    added

--- src/Oct_24/ex_21Oct_Forloops/lab_21Oct_Primenumbers.java
+++ src/Oct_24/ex_21Oct_Forloops/lab_21Oct_Primenumbers.java
@@ -1,11 +1,28 @@
 package Oct_24.ex_21Oct_Forloops;
 
-//Java program to print all Prime numbers between 1 to n using for loop.
-//public class lab_21Oct_Primenumbers {
-    public static void main(String[] args) {
-//        for (int i = 1; i < n; i++) {
-//            if ()
-//        }
- }
+import java.util.Scanner;
 
+//Java program to print all Prime numbers between 1 to n using for loop.
+public class lab_21Oct_Primenumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" please Enter Number");
+        int num = sc.nextInt();
+        int count = 0;
+
+        if (num > 1) {
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 4) {
+                System.out.println(" Number is prime number");
+
+            } else {
+                System.out.println(" Not a prime number");
+            }
+        }
+
+    }
+}
 
---
+++ src/Oct_24/ex_22Oct_Whileloops/lab_22Oct_practiceWhile.java
@@ -0,0 +1,16 @@
+package Oct_24.ex_22Oct_Whileloops;
+
+public class lab_22Oct_practiceWhile {
+    public static void main(String[] args) {
+//        int n;
+//        int i = 2;
+//        while (i <= (n - 1)) {
+//            if (n % i == 0){
+//                System.out.println("It's not prime number");
+//                break;
+//                } else {
+//                System.out.println("It's prime number");
+//            }
+//        }
+    }
+}
