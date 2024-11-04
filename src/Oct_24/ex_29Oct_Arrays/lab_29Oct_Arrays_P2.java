d90275c4255660108d3bbd11f9f4800e7446de3c
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-04 16:41:54 +0530

    addedNEW

---
+++ src/Oct_24/ex_29Oct_Arrays/lab_29Oct_Arrays_P2.java
@@ -0,0 +1,19 @@
+package Oct_24.ex_29Oct_Arrays;
+
+//By using New operator
+public class lab_29Oct_Arrays_P2 {
+    public static void main(String[] args) {
+        int[] marks_2 = new int[6];
+        System.out.println(marks_2.length);
+
+        marks_2[0] = 11;
+        marks_2[1] = 12;
+        marks_2[3] = 14;
+        marks_2[5] = 18;
+
+        System.out.println(marks_2[0]);
+        System.out.println(marks_2[3]);
+        System.out.println(marks_2[5]);
+
+    }
+}
--- src/Oct_24/ex_29Oct_Arrays/lab_29Oct_Arrays_practice.java
+++ src/Oct_24/ex_29Oct_Arrays/lab_29Oct_Arrays_practice.java
@@ -2,6 +2,16 @@ package Oct_24.ex_29Oct_Arrays;
 
 public class lab_29Oct_Arrays_practice {
     public static void main(String[] args) {
+        int[] marks = {51, 100, 91, 87, 90, 91, 92};
+        // 0 to 6
+        // Arrays Creation with the pre-defined elements.
+        System.out.println(marks[0]);
+        System.out.println(marks[4]);
+        System.out.println(marks[6]);
+        //System.out.println(marks[10]); // ArrayIndexOutOfBoundsException
+        //length always start from 1
+
+        System.out.println(marks.length); //length 1
 
     }
 }
