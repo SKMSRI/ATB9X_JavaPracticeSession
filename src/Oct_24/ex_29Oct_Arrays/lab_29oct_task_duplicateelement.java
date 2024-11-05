aaaa57769b8dc954cd62661526f5b60e7a266c84
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-05 20:44:36 +0530

    addedNEW

--- src/Oct_24/ex_29Oct_Arrays/lab_29oct_task_duplicateelement.java
+++ src/Oct_24/ex_29Oct_Arrays/lab_29oct_task_duplicateelement.java
@@ -2,7 +2,15 @@ package Oct_24.ex_29Oct_Arrays;
 
 public class lab_29oct_task_duplicateelement {
     public static void main(String[] args) {
-        int[] numbers ={24, 34, 67, 58, 34, 78, 55, 58};
+        int arr[] = new int[] {3, 5, 4, 6, 8, 6, 9, 4};
+        System.out.println("Duplicate arrary is");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j =i+1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[j]);
 
+                }
+            }
+        }
     }
 }
