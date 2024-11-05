97c8ec4cbe182ab36ca64cb9fd9cc5bdd9c5ec4d
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-05 19:51:40 +0530

    addedNEW

---
+++ src/Oct_24/ex_29Oct_Arrays/lab_29OCT_array_string.java
@@ -0,0 +1,12 @@
+package Oct_24.ex_29Oct_Arrays;
+
+public class lab_29OCT_array_string {
+    public static void main(String[] args) {
+        String[] name = {"SAURABH", "Pooja", "ABC"};
+        String[] name_batch = new String[3];
+        name_batch[0]="SAURABH";
+        name_batch[1] ="pooja";
+        name_batch[2] ="ABC";
+        System.out.println(name_batch.length);
+    }
+}
