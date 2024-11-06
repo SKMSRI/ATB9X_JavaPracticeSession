8e41986375e892810d8a7c645d0e33b3d11c4760
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-06 20:27:09 +0530

    06 Nov

---
+++ src/NOV_24/ex_06Nov_constructors/DefaultCons.java
@@ -0,0 +1,12 @@
+package NOV_24.ex_06Nov_constructors;
+
+public class DefaultCons {
+    public static void main(String[] args) {
+        DefaultCons d = new DefaultCons();//Default constructor
+        System.out.println("d");
+
+
+    }
+
+
+}
