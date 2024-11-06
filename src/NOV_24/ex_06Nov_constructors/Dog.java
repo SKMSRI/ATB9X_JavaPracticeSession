35329ae066f96f1e26a131e1ffd9293a38d78e71
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-06 20:21:15 +0530

    06 Nov

---
+++ src/NOV_24/ex_06Nov_constructors/Dog.java
@@ -0,0 +1,26 @@
+package NOV_24.ex_06Nov_constructors;
+
+  // Define the Dog class
+    public class Dog {
+        // Private instance variables
+        private String name;
+        private String color;
+
+        // Parameterized constructor
+        public Dog(String name, String color) {
+            // Initialize name with the provided parameter
+            this.name = name;
+            this.color = color;
+        }
+
+        // Main method to test the Dog class
+        public static void main(String[] args) {
+            // Create a new Dog object using the parameterized constructor
+            Dog myDog = new Dog("Bailey", "Black");
+            // Print the values of the instance variables
+            System.out.println("Dog's Name: " + myDog.name);
+            System.out.println("Dog's Color: " + myDog.color);
+        }
+    }
+
+
