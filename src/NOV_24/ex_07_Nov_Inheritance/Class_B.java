37ec194669160a25bd693bcb3de9a9a120a39f36
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-11 18:48:22 +0530

    11 Nov

---
+++ src/NOV_24/ex_07_Nov_Inheritance/Class_B.java
@@ -0,0 +1,15 @@
+package NOV_24.ex_07_Nov_Inheritance;
+
+public class Class_B extends Class_A {
+    public static void main(String[] args) {
+        Class_B af = new Class_B();
+        af.print_A();
+
+    }
+    void B(){
+        System.out.println("B");
+    }
+   }
+
+
+
