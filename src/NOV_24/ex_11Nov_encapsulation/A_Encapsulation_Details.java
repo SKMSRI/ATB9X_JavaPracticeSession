20a01c5af68f52e3f76945aded6c6774190bff24
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-13 20:25:39 +0530

    12 Nov

---
+++ src/NOV_24/ex_11Nov_encapsulation/A_Encapsulation_Details.java
@@ -0,0 +1,11 @@
+package NOV_24.ex_11Nov_encapsulation;
+
+public class A_Encapsulation_Details {
+}
+//Encapsulation is a core principle of object-oriented programming in Java that involves
+// bundling data (attributes) and methods (functions) that operate on that data into a
+// single unit, typically a class.
+//Private fields (name, age, salary) do not allow direct access.
+//Public methods (getName(), setName(), etc.) allow controlled access.
+//Additional validations can be added inside the setter methods to enforce business
+// rules (e.g., checking if age or salary is valid).
