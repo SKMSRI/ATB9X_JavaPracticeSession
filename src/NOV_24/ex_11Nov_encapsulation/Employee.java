04549aab090f62ed7bf0b31904d01c8b590914c2
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-14 19:32:57 +0530

    14 Nov

---
+++ src/NOV_24/ex_11Nov_encapsulation/Employee.java
@@ -0,0 +1,16 @@
+package NOV_24.ex_11Nov_encapsulation;
+
+public class Employee {
+    private int empid;
+//getter and setter method
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+
+}
+//Get for view the vlaues and set to modify
---
+++ src/NOV_24/ex_11Nov_encapsulation/company.java
@@ -0,0 +1,9 @@
+package NOV_24.ex_11Nov_encapsulation;
+
+public class company {
+    public static void main(String[] args) {
+         Employee e= new Employee();
+         e.setEmpid(101);
+        System.out.println(e.getEmpid());
+    }
+}
