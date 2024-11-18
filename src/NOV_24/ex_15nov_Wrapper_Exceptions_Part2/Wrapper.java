418407f7d17d7e9071a011f5dacce674110be1d0
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-18 20:09:24 +0530

    14 Nov

---
+++ src/NOV_24/ex_15nov_Wrapper_Exceptions_Part2/Wrapper.java
@@ -0,0 +1,18 @@
+package NOV_24.ex_15nov_Wrapper_Exceptions_Part2;
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        int a = 10;
+        // Primitive Data type
+        // Java hates it ->
+        // Java Loves - class and Object.
+        String s1 = new String("pramod");
+        //s1.contains("a");
+
+        Integer a1 = 10;
+        // a1.intValue(); - Class and Object - Java loves.
+
+        // Why Java is not pure OOPs? - Becoz -> primitive data types.
+
+
