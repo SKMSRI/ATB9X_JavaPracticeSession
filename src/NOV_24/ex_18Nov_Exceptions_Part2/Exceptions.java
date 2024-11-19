d605abeff7e063810313a4d6a0a94d3ec72cf088
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-19 17:12:07 +0530

    15 Nov

--- src/NOV_24/ex_18Nov_Exceptions_Part2/Exceptions.java
+++ src/NOV_24/ex_18Nov_Exceptions_Part2/Exceptions.java
@@ -1,5 +1,21 @@
 package NOV_24.ex_18Nov_Exceptions_Part2;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        int a = 0;
+        int c = 0;
+        try {
+            c = 10/a;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(c); // java.lang.ArithmeticException
+    }
 
 }
