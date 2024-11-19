99a61c4e21648cfdf865698197b53abe1d3d2a05
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-19 17:14:13 +0530

    15 Nov

---
+++ src/NOV_24/ex_18Nov_Exceptions_Part2/Exceptions_finallyakeyword.java
@@ -0,0 +1,19 @@
+package NOV_24.ex_18Nov_Exceptions_Part2;
+
+public class Exceptions_finallykeyword {
+    public static void main(String[] args) {
+        Scanner sc = null;
+        sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        try {
+            int a = 10 / 0;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            sc.close();
+            System.out.println("closing sc");
+
+        }
+
+    }
+}
