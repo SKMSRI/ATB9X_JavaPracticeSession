99afb722c1f9a391e143131211e36aea5fb4bba1
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-20 19:52:24 +0530

    15 Nov

--- src/NOV_24/ex_12Nov_Abstraction/lab_12Nov_abstraction.java
+++ src/NOV_24/ex_12Nov_Abstraction/lab_12Nov_abstraction.java
@@ -1,4 +1,16 @@
 package NOV_24.ex_12Nov_Abstraction;
-
+/*
+Data abstraction is the process of hiding certain details and showing only essential
+information to the user.
+Abstraction can be achieved with either abstract classes or interfaces.
+The abstract keyword is a non-access modifier, used for classes and methods.
+Abstract class:
+ is a restricted class that cannot be used to create objects
+ (to access it, it must be inherited from another class).
+Abstract method:
+can only be used in an abstract class, and it does not have a body.
+The body is provided by the subclass (inherited from).
+*/
 public class lab_12Nov_abstraction {
+
 }
