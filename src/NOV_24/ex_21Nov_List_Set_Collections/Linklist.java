e2944e35119786a41e387616112f9225f390ae9d
SAURABH <er.saurabhkumar22@gmail.com>
2024-11-22 19:18:51 +0530

    21Nov

---
+++ src/NOV_24/ex_21Nov_List_Set_Collections/Linklist.java
@@ -0,0 +1,21 @@
+package NOV_24.ex_21Nov_List_Set_Collections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Linklist {
+    public static void main(String[] args) {
+        //LinkedList mylist = new LinkedList();
+        List mylist = new LinkedList();
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(4);
+        System.out.println(mylist);
+
+        //  Selenium -  -> Store and Insertion and Deletion - DS - LL
+        // API Testing - STORE, access it fast. -> AL
+}
+
+}
